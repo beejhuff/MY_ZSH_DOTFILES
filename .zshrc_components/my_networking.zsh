44992a043f50904cbb6bef0b085b11b8db8efe1c
dbkvn <kevinmartinucr@gmail.com>
2016-06-17 12:33:50 -0700

    Updated my_networking.zsh with a diagnostic function.

--- .zshrc_components/my_networking.zsh
+++ .zshrc_components/my_networking.zsh
@@ -3,7 +3,13 @@
 # Written by Kevin Martin.
 # This set of aliases and functions is used for networking settings, etc.
 
-# Don't send continuous pings.
+# Call iftop with as superuser by default (Personal machine only!)
+alias iftop="sudo /usr/local/sbin/iftop"
+
+# List open connections using the lsof command
+alias connections="sudo lsof -n -i -P -c 15"
+
+# Stop after sending count ECHO_REQUEST packets #
 alias ping="ping -c 5"
 
 # Use an alias for the Apple airport utility
@@ -16,8 +22,27 @@ alias airportcycle='networksetup -setairportpower en0 off; sleep 1; networksetup
 alias wifion='networksetup -setairportpower en0 on'
 alias wifioff='networksetup -setairportpower en0 off'
 
-# Call iftop with as superuser by default (Personal machine only!)
-alias iftop="sudo /usr/local/sbin/iftop"
-
-# List open connections using the lsof command
-alias connections="sudo lsof -n -i -P -c 15"
+# # # # # My Network Info Function # # # # #
+# This is a script to print detailed information about each hardware port on
+# my Mac computer. This uses the networksetup utility to display information.
+my_networking_info() {
+    echo Printing all network services...
+    echo ------------------------------------------------------------
+    networksetup -listallnetworkservices
+    echo ------------------------------------------------------------
+    ports=('Thunderbolt FireWire' 'Wi-Fi' 'Bluetooth PAN' 'Thunderbolt Bridge')
+    echo
+    echo Printing information about network services...
+    echo
+    for port in $ports; do
+        echo Information about $port:;
+        echo ------------------------------------------------------------
+        networksetup -getinfo $port
+        echo ------------------------------------------------------------
+        echo
+    done
+    echo Printing information about current wireless status...
+    echo ------------------------------------------------------------
+    /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo
+    echo ------------------------------------------------------------
+}
